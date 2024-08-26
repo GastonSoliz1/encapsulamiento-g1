c00b630744a304d96351bbc1b28d6ca9d4bcaac1
Luis Eduardo Torri Aguilar <letorri@gyl.local>
2024-08-26 15:50:12 -0300

    borrando carpeta indeseada

--- Encapsulamiento/src/main/java/Encapsulamiento/Main.java
+++
@@ -1,8 +0,0 @@
-package Encapsulamiento;
-
-public class Main {
-    public static void main(String[] args) {
-        Aceleradores acelerador = new Aceleradores(1, "Carlos 1","Arellano");
-        System.out.println("El nombre es: "+acelerador.getNombre()+" "+acelerador.getApellido());
-    }
-}
--- Encapsulamiento/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.encapsulamiento {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.encapsulamiento to javafx.fxml;
-    exports com.example.encapsulamiento;
-}
